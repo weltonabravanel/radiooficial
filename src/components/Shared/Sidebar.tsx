7fb4c9ebdac968c5be2a2c01b37540bb700e9e71
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2025-04-28 15:49:43 -0300

    Update Sidebar.tsx

--- src/components/Shared/Sidebar.tsx
+++ src/components/Shared/Sidebar.tsx
@@ -4,12 +4,13 @@ import { NavLink } from "react-router-dom";
 import { Switch } from "../ui/switch";
 import { useTheme } from "../../contexts/theme-provider";
 import { Button } from "../ui/button";
+import { DiCodeigniter } from "react-icons/di";
 
 const links = [
   { name: "Navegar", path: "/", icon: <BoomBox /> },
   { name: "Rádio Map", path: "/radio-map", icon: <Globe2 /> },
   { name: "Favoritos", path: "/favorites", icon: <FolderHeart /> },
-  { name: "Whatsapp", path: "https://wa.me/5531982845056?text=Oi%20quero%20adicionar%20minha%20r%C3%A1dio", icon: <DiAndroid /> }, 
+  { name: "Whatsapp", path: "https://wa.me/5531982845056?text=Oi%20quero%20adicionar%20minha%20r%C3%A1dio", icon: <DiCodeigniter /> }, 
 ];
 
 const Sidebar = () => {
